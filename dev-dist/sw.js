a787ac195f720ae65e8e246fc83c7461bc3375fb
Ahmed Nageh <ahmed.nageh@example.com>
2025-12-15 12:37:10 +0000

    chore: Update build artifacts after landing page fix

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q5qf36a980o"
+    "revision": "0.1ngluf7t29g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
